eedf5e32df2b7223307984bcbdadc1d1b5caf1d1
Brown <brownmr1@jhuapl.edu>
2017-11-08 13:40:02 -0500

    refactor: made Preprocessor more modular

--- Preprocessor.py
+++ Preprocessor.py
@@ -1,50 +1,34 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
 
-housing = pd.read_csv("train.csv")
-raw1 = pd.read_csv("train.csv")
-y = housing.SalePrice.values
 
-def numberClean():
-    housing.drop("SalePrice",1,inplace=True)
-    isNumeric = ((housing.dtypes == "int64") | (housing.dtypes == "float64")).values
-    numericColumns = housing.columns[isNumeric]
-    colCount = numericColumns.shape[0]
-    for colName in numericColumns:
-        column = housing[colName].values
+def number_clean(df):
+    # df.drop("SalePrice", 1, inplace=True)
+    df.drop(df.columns[len(df.columns) - 1], axis=1, inplace=True)
+    is_numeric = ((df.dtypes == "int64") | (df.dtypes == "float64")).values
+    numeric_columns = df.columns[is_numeric]
+    for col_name in numeric_columns:
+        column = df[col_name].values
         median = np.nanmedian(column)
-        housing.at[np.isnan(column),colName] = median
-    #Sums number of NaN values:
-        #print(np.sum(np.isnan(isNumeric)))
-    #print(numericColumns)
-    #print(objectColumns)
+        df.at[np.isnan(column), col_name] = median
 
 
-def objectClean():
-    isObject = (housing.dtypes == "object").values
-    objectColumns = housing.columns[(isObject)]
-    colCount = objectColumns.shape[0]
-    X = housing[objectColumns].values
-    housing['CentralAir'] = np.where(housing.CentralAir=='Y',1,0)
-    #print(housing.PavedDrive)
-    housing.PavedDrive = np.where(housing.PavedDrive=='Y',1,0)
-    #print(housing.PavedDrive)
+def object_clean(df):
+    df.CentralAir = np.where(df.CentralAir == 'Y', 1, 0)
+    df.PavedDrive = np.where(df.PavedDrive == 'Y', 1, 0)
 
 
-def clean():
-    objectClean()
-    numberClean()
-    housing['SalePrice'] = y
-    housing.to_csv("cleanTrain.csv", index=False)
-    newHousing = pd.read_csv("cleanTrain.csv")
+def clean(df, og):
+    object_clean(df)
+    number_clean(df)
+    df[og] = y
+    df.to_csv("cleanTrain.csv", index=False)
+    return df
 
 
-
-clean()
-#print(raw)
-#print(housing.PavedDrive.value_counts())
-#objectClean()
-#numberClean()
+if __name__ == '__main__':
+    housing = pd.read_csv("train.csv")
+    raw1 = pd.read_csv("train.csv")
+    y = housing.SalePrice.values
+    y_name = 'SalePrice'
+    print(clean(housing, y_name))
