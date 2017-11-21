564714f5562611c5d54b81a438411dd0b12afec8
Brown <brownmr1@jhuapl.edu>
2017-11-21 13:23:17 -0500

    fix: not working as well as before

--- Evaluator.py
+++ Evaluator.py
@@ -1,10 +1,12 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+from matplotlib.pyplot import plot
 from sklearn import svm
-from sklearn.feature_selection import chi2
-from sklearn.linear_model import LogisticRegression, Lasso, LinearRegression
-from sklearn.metrics import mean_squared_error, make_scorer , mean_absolute_error, median_absolute_error, r2_score, mean_squared_log_error, explained_variance_score
+from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_selection import chi2
+from sklearn.linear_model import Lasso
+from sklearn.metrics import mean_squared_error, make_scorer , mean_absolute_error, median_absolute_error, r2_score, mean_squared_log_error, explained_variance_score
 from sklearn.model_selection import train_test_split, GridSearchCV
 
 
@@ -45,35 +47,34 @@ if __name__ == "__main__":
 # Set metric
     # metric = mean_squared_error
     # metric = median_absolute_error
-    # metric = mean_absolute_error
     metric = mean_squared_log_error
     # metric = r2_score
     # metric = explained_variance_score
 # Set model
-    # model = LinearRegression()
     model = RandomForestRegressor()
-    # model = LogisticRegression()
     # model = Lasso()
 # Setting data variables
     X = housing_train.values  # Shape: (1460, 79)
     y = pd.read_csv("train_prices.csv").values  # Shape: (1460, 1)
     test_x = housing_test.values  # Shape: (1459, 79)
-    price_true = pd.read_csv("train_prices.csv")
+    y_col = pd.read_csv("train_prices.csv")
 # train_test_split validation
     X_train, X_val, y_train, y_val = train_test_split(
        X, y, test_size=0.33, random_state=42)    # X_train.shape = (978, 79)    # y_train.shape = (978, 1)
-    grid_search(X_train, y_train)
-    # model.fit(X_train, y_train)
-    # y_pred = model.predict(X_val)
-    # y_val = y_val.reshape(y_val.shape[:1])
-    # print(metric(y_val, y_pred))
-    #
-    # model.fit(X, y)
-    # price_pred = model.predict(test_x)
-    # index = housing_test.index
-    # pred_df = pd.DataFrame(index=index)
-    # # pred_df['SalePrice'] = price_pred
-    # # pred_df.to_csv("Submission.csv")
+#     grid_search(X_train, y_train)
+    pca = PCA(n_components=40, whiten=True)
+    X_train_pca = pca.fit_transform(X_train)    # Shape: (978, 5)
+    model.fit(X_train_pca, y_train)
+    new_x_val = pca.transform(X_val)  # Shape: (482, 5)
+    y_val_pred = model.predict(new_x_val)
+    y_val = y_val.reshape(y_val.shape[:1])
+    metric(y_val, y_val_pred)
+    new_test_x = pca.transform(test_x)
+    price_pred = model.predict(new_test_x)
+    index = housing_test.index
+    pred_df = pd.DataFrame(index=index)
+    pred_df['SalePrice'] = price_pred
+    pred_df.to_csv("Submission.csv")
 
 
 # EXTRAS #
@@ -89,34 +90,39 @@ if __name__ == "__main__":
 TESTING
 
 Model: RandomForest
-    Metric: MedianAbs: 11,549.0 - 11,189.2 - 11,876.35
-    Metric: MeanAbs: 19,100.3680498
-    Metric: MeanSq: 982,556,345.949 - 1,358,389,203.78
-    Metric: ExplVar: 0.841691548145
-    Metric: MeanSqLog: 0.021788982229 - 0.0235023178184 <---- WINNER
-    Metric: R2: 0.838998381662
+    Metric: MedianAbs(0): 13,895
+    Metric: MeanSq(0): 1,757,965,444.23
+    Metric: ExplVar(1): 0.841691548145
+    Metric: MeanSqLog(0): 0.0380640543864
+    Metric: R2(1): 0.838998381662
 
 Model: Lasso
-    Metric: MedianAbs: 16,029.5013744
-    Metric: MeanAbs: 21,906.9826906
-    Metric: MeanSq: 1,192,649,897.97
+    Metric: MedianAbs: 15,336.756
+    Metric: MeanSq: 1,403,132,097.4
     Metric: ExplVar: 0.838225609045
-    Metric: MeanSqLog: ERROR
-    Metric: R2: 0.837544432739
-
-Model: LogReg
-    Metric: MedianAbs: 26,250.0
-    Metric: MeanAbs: 40,846.033195
-    Metric: MeanSq: 4,173,891,592.33
-    Metric: ExplVar: 0.43569662705
-    Metric: MeanSqLog: 0.0959748166812
-    Metric: R2: 0.431457691422
-
-Model: LinReg
-    Metric: MedianAbs: 16,023.6214121
-    Metric: MeanAbs: 21,910.0993854
-    Metric: MeanSq: 1,193,264,065.64
-    Metric: ExplVar: 0.83814249713
-    Metric: MeanSqLog: ERROR
-    Metric: R2: 0.837460774527
+    Metric: MeanSqLog: 0.0346569581418
+    Metric: R2: 0.837544432739  (Approach 1)
+    
+RandomFor/MeanSqLog/PCA:
+    components = 5: 0.0456637532179
+    components = 10: 0.0420794780193
+    components = 15: 0.0396171025027
+    components = 20: 0.0381703190471
+    components = 25: 0.0389053574836
+    components = 30: 0.0358936883713
+    components = 35: 0.0357876535382
+    components = 40: 0.0357587856916
+    components = 50: 0.0365345471203
+    components = 79: 0.0373731851169
+        Whiten = True:
+        5: 0.0445929599005
+        10: 0.0436346260886
+        15: 0.0377110578718
+        20: 0.0397212720809
+        25: 0.0387014142115
+        30: 0.0375210273287
+        35: 0.0371114498236
+        40: 0.0342316306501 <--- WINNER
+        45: 0.0362241648409
+        79: 0.0385043050604
 '''
