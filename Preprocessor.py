5169ee409e635023bc2f4c99984238c5d0107f19
Brown <brownmr1@jhuapl.edu>
2017-12-12 14:10:41 -0500

    feat: add test back and remove unused functions

--- Preprocessor.py
+++ Preprocessor.py
@@ -11,16 +11,10 @@ def number_clean(df):
         df.at[np.isnan(column), col_name] = median
 
 
-# def object_clean(df):
-#     df.CentralAir = np.where(df.CentralAir == ['Y', 1, 0])
-#     df.PavedDrive = np.where(df.PavedDrive == ['Y', 1, 0])
-
-
 def clean(df, is_train):
-    # object_clean(df)
     number_clean(df)
     if is_train:
-        df.to_csv("cleanTrain.csv", index=False)  # Index is reset each new csv
+        df.to_csv("cleanTrain.csv", index=True)  # Index is reset each new csv
     else:
         df.to_csv("cleanTest.csv", index=True)  # Want to keep index to keep order
     return df
@@ -35,7 +29,7 @@ def run(csv, is_train):
         prices_df.to_csv('train_prices.csv', index=False)
         housing.drop('SalePrice', 1, inplace=True)
     data = clean(housing, is_train)
-    print(data)
+    print(data['LotConfig'].head(15))
 
 
 if __name__ == '__main__':
