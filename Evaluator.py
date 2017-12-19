in the process of fixing scaling errors

Evaluator.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from sklearn import svm
 from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import median_absolute_error, mean_squared_log_error
+from sklearn.metrics import mean_squared_log_error
 from sklearn.model_selection import train_test_split, GridSearchCV
 
 
@@ -72,6 +72,7 @@ if __name__ == "__main__":
     time_start = datetime.now()
     print(str(time_start), "\n")
 
+    # Initialize DataFrames
     housing_train = pd.read_csv("featurized_train.csv")  # Train Shape: (1460, 195)
     housing_test = pd.read_csv("featurized_test.csv")  # Test Shape: (1459, 195)
     housing_test.set_index('Id', drop=True, inplace=True)
@@ -82,48 +83,44 @@ if __name__ == "__main__":
         if column not in housing_test.columns:
             housing_train.drop(column, 1, inplace=True)
 
+    # Initialize variables
     metric = mean_squared_log_error
     model = RandomForestRegressor()
     X = housing_train.values  # Shape: (1460, 195)
     y = pd.read_csv("train_prices.csv").values  # Shape: (1460,)
     y = np.squeeze(y)
     X_test = housing_test.values  # Shape: (1459, 195)
-
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.33, random_state=42)
     y_train = np.squeeze(y_train)
     y_val = np.squeeze(y_val)
     # X_train.shape = (978, 195) / X_val.shape = (482, 195) / y_train.shape = (978,) / y_val.shape = (482,)
 
-    # quick_X, quick_y, quick_xval, quick_yval = X[:150], y[:150], X[-150:], y[-150:]
-    # best_c, best_kernel, best_gamma, best_score = grid_search(quick_X, quick_y, quick_xval, quick_yval)
-    best_c, best_kernel, best_gamma,  best_score = grid_search(X_train, y_train, X_val, y_val)
-    svr = svm.SVR(C=best_c, kernel=best_kernel, gamma=best_gamma)
-    svr.fit(X_train, y_train)  # svr.fit(quick_X, quick_y)
-    print("SVR Score: ", svr.score(X_val, y_val))  # print("SVR Score: ", svr.score(quick_xval, quick_yval))
+    # # FILLER FOR GRID SEARCH CODE # #
 
-    '''
+    # Shows n_components/score plot
+    comp_score_plot(X_train, y_train, X_val, y_val)
+
+    # PCA Transform
     pca = PCA(n_components=41, whiten=True)
     X_train_pca = pca.fit_transform(X_train)    # Shape: (978, n_components)
+    X_val_pca = pca.transform(X_val)  # Shape: (482, n_components)
+
+    # Model testing
     model.fit(X_train_pca, y_train)
-    new_x_val = pca.transform(X_val)  # Shape: (482, n_components)
-    y_val_pred = model.predict(new_x_val)  # Shape: (482,)
+    val_pred = model.predict(X_val_pca)  # Shape: (482,)
     y_val = y_val.reshape(y_val.shape[:1])  # Shape: (482,)
-    
-    # print("Pred: \n", y_val_pred, "\nGround Truth: \n", y_val)
+
     # quick_pred_df = pd.DataFrame({'Price': y_val_pred})
     # quick_pred_df.to_csv('Quick_Pred.csv')
     # quick_truth_df = pd.DataFrame({'Price': y_val})
     # quick_truth_df.to_csv('Quick_Truth.csv')
     
-    print(metric(y_val, y_val_pred))  # median_absolute: 14112.5 / mean_squared_log: .03573
+#    print(metric(y_val, val_pred))  # median_absolute: 14112.5 / mean_squared_log: .03573
     
     # X_pca = pca.fit_transform(X)
     # y = np.squeeze(y)
     # model.fit(X_pca, y)
     # new_test_x = pca.transform(X_test)
-    '''
-
-#    comp_score_plot(X_train, y_train, X_val, y_val)
 
 #    svr.fit(X, y)
 #    prediction = svr.predict(X_test)
@@ -136,13 +133,21 @@ if __name__ == "__main__":
     print("\n", str(time_done), "\n")
     time_elapsed(time_start, time_done)
 
-'''Test Results
-    Quick Set
-      Size 50: C=1/kernel='linear'/gamma='auto'/Best Score: 0.696984862476
-      Size 100: C=1/kernel='linear'/gamma='auto'/Best Score: 0.645307692251
-      Size 150: C=100/kernel='linear'/gamma='auto'/Best Score: 0.762856090685
-    Grid Search (Sample Size/Time Run)
-      50  / 1 min, 35.570 sec
-      100 / 6 min, 32.175 sec # Up about 5
-      150 / 12 min, 38.260 sec # By 6? Maybe linear?
+# Test Results
+#     Quick Set
+#       Size 50: C=1/kernel='linear'/gamma='auto'/Best Score: 0.696984862476
+#       Size 100: C=1/kernel='linear'/gamma='auto'/Best Score: 0.645307692251
+#       Size 150: C=100/kernel='linear'/gamma='auto'/Best Score: 0.762856090685
+#     Grid Search (Sample Size/Time Run)
+#       50  / 1 min, 35.570 sec
+#       100 / 6 min, 32.175 sec # Up about 5
+#       150 / 12 min, 38.260 sec # By 6? Maybe linear?
+
+'''
+# quick_X, quick_y, quick_xval, quick_yval = X[:150], y[:150], X[-150:], y[-150:]
+# best_c, best_kernel, best_gamma, best_score = grid_search(quick_X, quick_y, quick_xval, quick_yval)
+best_c, best_kernel, best_gamma,  best_score = grid_search(X_train, y_train, X_val, y_val)
+svr = svm.SVR(C=best_c, kernel=best_kernel, gamma=best_gamma)
+svr.fit(X_train, y_train)  # svr.fit(quick_X, quick_y)
+print("SVR Score: ", svr.score(X_val, y_val))  # print("SVR Score: ", svr.score(quick_xval, quick_yval))
 '''

Featurizer2.py
@@ -1,33 +1,28 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
 
 def encode(train_df, test_df, enc):
     # Creates subset of DataFrames with only object columns
-    train_obj = train_df.select_dtypes(include=['object'])
-    test_obj = test_df.select_dtypes(include=['object'])
-    col_names = train_obj.columns
-
-    '''
-    # Sums up the nulls in each column
-    train_nulls = train_obj.isnull().sum()
-    test_nulls = test_obj.isnull().sum()
-    '''
+    train_object = train_df.select_dtypes(include=['object'])  # Index starts at 0
+    test_object = test_df.select_dtypes(include=['object'])
+    col_names = train_object.columns
 
     # Creates and transposes matrices to represent DataFrames
-    train = train_obj.values
-    train = np.matrix.transpose(train)
-    test = test_obj.values
-    test = np.matrix.transpose(test)
+    train_vals = train_object.values
+    train_vals = np.matrix.transpose(train_vals)
+    test_vals = test_object.values
+    test_vals = np.matrix.transpose(test_vals)
     # LabelEncodes both matrices
-    for x in range(len(train)):
+    for x in range(len(train_vals)):
         # print(np.unique(train[x].astype(str), return_counts=True))  # Essentially value_counts for an array
-        train[x] = enc.fit_transform(train[x].astype(str))
-        if x <= (len(test)-1):
-            test[x] = enc.transform(test[x].astype(str))
-    return train, test, col_names
+        train_vals[x] = enc.fit_transform(train_vals[x].astype(str))
+        if x <= (len(test_vals)-1):
+            test_vals[x] = enc.transform(test_vals[x].astype(str))
+    return train_vals, test_vals, col_names
 
+# # # THERE'S A NAN IN MY CODE # # #
 
 # Turns matrices back into DataFrames and adds index for test
 def re_dataframe(train_mat, test_mat):
@@ -61,31 +56,62 @@ def rejoin(full_train, full_test, train_dum, test_dum):
 
 if __name__ == '__main__':
     encoder = LabelEncoder()
+    scaler = MinMaxScaler()
 
-    # Creates DataFrame and preserves index for Train
-    train_dataframe = pd.read_csv("cleanTrain.csv")
+    # Creates DataFrames and preserves index
+    train_dataframe = pd.read_csv("cleanTrain.csv")  # (1460, 72)
     if 'Id' in train_dataframe.columns:
         train_dataframe.set_index('Id', drop=True, inplace=True)
     index_train = train_dataframe.index
-
-    # Now Test
-    test_dataframe = pd.read_csv("cleanTest.csv")
+    test_dataframe = pd.read_csv("cleanTest.csv")  # (1459, 72)
     if 'Id' in test_dataframe.columns:
         test_dataframe.set_index('Id', drop=True, inplace=True)
     index_test = test_dataframe.index
 
-    # Drops columns with additional categories in Test
-    train_dataframe.drop(['MSZoning', 'Utilities', 'Exterior1st', 'Exterior2nd', 'KitchenQual', 'Functional',
-                          'SaleType'], 1, inplace=True)
-    test_dataframe.drop(['MSZoning', 'Utilities', 'Exterior1st', 'Exterior2nd', 'KitchenQual', 'Functional',
-                         'SaleType'], 1, inplace=True)
+    # Drops columns with categories in Test but not Train
+    train_dataframe.drop(['MSZoning', 'Utilities', 'Exterior1st', 'Exterior2nd', 'KitchenQual', 'Functional', 'SaleType'
+                          ], 1, inplace=True)
+    test_dataframe.drop(['MSZoning', 'Utilities', 'Exterior1st', 'Exterior2nd', 'KitchenQual', 'Functional', 'SaleType'
+                         ], 1, inplace=True)
 
-    # Passes DataFrames through methods
+    # Variables
+    X = train_dataframe.values
+    y = pd.read_csv("train_prices.csv")
+    y = y.values
+
+    # Separate object from number
+    train_obj = train_dataframe.select_dtypes(include=['object'])
+    train_num = train_dataframe.drop(train_obj, 1)  # (1460, 36)
+    test_obj = test_dataframe.select_dtypes(include=['object'])
+    test_num = test_dataframe.drop(test_obj, 1)  # (1459, 36)
+    train_cols = train_num.columns
+    test_cols = test_num.columns
+
+    # Start scaling (to between 1 and 0)
+
+    scaled_train = scaler.fit_transform(train_num)  # (1460, 36)
+    scaled_test = scaler.transform(test_num)  # (1459, 36)
+    train = pd.DataFrame(scaled_train, columns=train_cols)
+    test = pd.DataFrame(scaled_test, columns=test_cols, index=index_test)
+    train_dataframe = train.join(train_obj)
+    test_dataframe = test.join(test_obj)
+
+    # Label encodes category columns
     encoded_train, encoded_test, columns = encode(train_dataframe, test_dataframe, encoder)
-    new_train, new_test = re_dataframe(encoded_train, encoded_test)
-    train_dummy, test_dummy = one_hot(new_train, new_test)
-    mini_dummy = train_dummy[['LotConfig_1', 'LotConfig_2', 'LotConfig_3', 'LotConfig_4']]
-    train_dataframe, test_dataframe = rejoin(train_dataframe, test_dataframe, train_dummy, test_dummy)
 
-    train_dataframe.to_csv('featurized_train.csv', index=True)
-    test_dataframe.to_csv('featurized_test.csv', index=True)
+    # Turn encoded matrix back into DataFrame
+    new_train, new_test = re_dataframe(encoded_train, encoded_test)
+
+    # One-hot encodes
+    train_dummy, test_dummy = one_hot(new_train, new_test)  # Train: (1460, 194) / Test: (1459, 159)
+
+    # print(type(train_dataframe))
+    # print(train_dataframe.dtypes)
+
+    # Rejons one-hot with numeric
+    train_dataframe, test_dataframe = rejoin(train_dataframe, test_dataframe, train_dummy, test_dummy)
+    # train_dataframe.to_csv("temp.csv")
+
+    # # Finishes with a CSV
+    # train_dataframe.to_csv('featurized_train.csv', index=True)
+    # test_dataframe.to_csv('featurized_test.csv', index=True)