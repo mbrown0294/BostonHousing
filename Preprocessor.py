683982e54da2476b640aeae505c185762fb2c005
Brown <brownmr1@jhuapl.edu>
2017-12-21 15:08:25 -0500

    fix:refactoring

--- Preprocessor.py
+++ Preprocessor.py
@@ -14,9 +14,9 @@ def number_clean(df):
 def clean(df, is_train):
     number_clean(df)
     if is_train:
-        df.to_csv("cleanTrain.csv", index=True)  # Index is reset each new csv
+        df.to_csv("clean_train.csv", index=True)  # Index is reset each new csv
     else:
-        df.to_csv("cleanTest.csv", index=True)  # Want to keep index to keep order
+        df.to_csv("clean_test.csv", index=True)  # Want to keep index to keep order
     return df
 
 
@@ -26,10 +26,9 @@ def run(csv, is_train):
     if 'SalePrice' in housing.columns:
         prices = housing.SalePrice.values
         prices_df = pd.DataFrame({"SalePrice": prices})  # Change 'prices' into a df with column name and everything
-        prices_df.to_csv('train_prices.csv', index=False)
+        prices_df.to_csv('ground_truth.csv', index=False)
         housing.drop('SalePrice', 1, inplace=True)
     data = clean(housing, is_train)
-    print(data['LotConfig'].head(15))
 
 
 if __name__ == '__main__':
