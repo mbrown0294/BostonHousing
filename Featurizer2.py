0361ee51c9e06ec178c5285b0e95cd97d3765ad4
Brown <brownmr1@jhuapl.edu>
2017-12-21 13:31:31 -0500

    feat:added one-hot, new CSVs

--- Featurizer2.py
+++ Featurizer2.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
@@ -28,10 +29,10 @@ def encode(train_df, test_df, enc):
 def re_dataframe(train_mat, test_mat):
     train_mat = np.matrix.transpose(train_mat)
     test_mat = np.matrix.transpose(test_mat)
-    new_train_df = pd.DataFrame(train_mat, index=index_train)
-    new_test_df = pd.DataFrame(test_mat, index=index_test)
-    new_train_df.columns = columns
-    new_test_df.columns = columns
+    new_train_df = pd.DataFrame(train_mat, index=ind_train)
+    new_test_df = pd.DataFrame(test_mat, index=ind_test)
+    new_train_df.columns = cols
+    new_test_df.columns = cols
     return new_train_df, new_test_df
 
 
@@ -59,59 +60,55 @@ if __name__ == '__main__':
     scaler = MinMaxScaler()
 
     # Creates DataFrames and preserves index
-    train_dataframe = pd.read_csv("cleanTrain.csv")  # (1460, 72)
-    if 'Id' in train_dataframe.columns:
-        train_dataframe.set_index('Id', drop=True, inplace=True)
-    index_train = train_dataframe.index
-    test_dataframe = pd.read_csv("cleanTest.csv")  # (1459, 72)
-    if 'Id' in test_dataframe.columns:
-        test_dataframe.set_index('Id', drop=True, inplace=True)
-    index_test = test_dataframe.index
+    train = pd.read_csv("cleanTrain.csv")  # (1460, 72)
+    if 'Id' in train.columns:
+        train.set_index('Id', drop=True, inplace=True)
+    ind_train = train.index  # (1, 1460)
+    test = pd.read_csv("cleanTest.csv")  # (1459, 72)
+    if 'Id' in test.columns:
+        test.set_index('Id', drop=True, inplace=True)
+    ind_test = test.index
 
     # Drops columns with categories in Test but not Train
-    train_dataframe.drop(['MSZoning', 'Utilities', 'Exterior1st', 'Exterior2nd', 'KitchenQual', 'Functional', 'SaleType'
-                          ], 1, inplace=True)
-    test_dataframe.drop(['MSZoning', 'Utilities', 'Exterior1st', 'Exterior2nd', 'KitchenQual', 'Functional', 'SaleType'
-                         ], 1, inplace=True)
-
+    dropped = ['MSZoning', 'Utilities', 'Exterior1st', 'Exterior2nd', 'KitchenQual', 'Functional', 'SaleType']
+    train, test = train.drop(dropped, 1, inplace=False), test.drop(dropped, 1, inplace=False)
     # Variables
-    X = train_dataframe.values
+    X = train.values
     y = pd.read_csv("train_prices.csv")
-    y = y.values
+    y = np.squeeze(y.values)
+    columns = train.columns
 
-    # Separate object from number
-    train_obj = train_dataframe.select_dtypes(include=['object'])
-    train_num = train_dataframe.drop(train_obj, 1)  # (1460, 36)
-    test_obj = test_dataframe.select_dtypes(include=['object'])
-    test_num = test_dataframe.drop(test_obj, 1)  # (1459, 36)
-    train_cols = train_num.columns
-    test_cols = test_num.columns
+    # Separate object from number <------ No NaNs
+    obj_train = train.select_dtypes(include=['object'])
+    obj_test = test.select_dtypes(include=['object'])
+    num_train = train.drop(obj_train, 1)  # (1460, 36)
+    num_test = test.drop(obj_test, 1)  # (1459, 36)
+    num_columns = num_train.columns
+    obj_columns = obj_train.columns
 
-    # Start scaling (to between 1 and 0)
-
-    scaled_train = scaler.fit_transform(train_num)  # (1460, 36)
-    scaled_test = scaler.transform(test_num)  # (1459, 36)
-    train = pd.DataFrame(scaled_train, columns=train_cols)
-    test = pd.DataFrame(scaled_test, columns=test_cols, index=index_test)
-    train_dataframe = train.join(train_obj)
-    test_dataframe = test.join(test_obj)
+    # Start scaling (to between 1 and 0) <------ No NaNs
+    scaled_train = scaler.fit_transform(num_train)  # (1460, 36)
+    scaled_test = scaler.transform(num_test)  # (1459, 36)
+    scaled_train_num = pd.DataFrame(scaled_train, columns=num_columns, index=ind_train)
+    scaled_test_num = pd.DataFrame(scaled_test, columns=num_columns, index=ind_test)
+    train = scaled_train_num.join(obj_train)
+    test = scaled_test_num.join(obj_test)
 
     # Label encodes category columns
-    encoded_train, encoded_test, columns = encode(train_dataframe, test_dataframe, encoder)
+    encoded_train, encoded_test, cols = encode(obj_train, obj_test, encoder)
 
     # Turn encoded matrix back into DataFrame
     new_train, new_test = re_dataframe(encoded_train, encoded_test)
 
     # One-hot encodes
-    train_dummy, test_dummy = one_hot(new_train, new_test)  # Train: (1460, 194) / Test: (1459, 159)
-
-    # print(type(train_dataframe))
-    # print(train_dataframe.dtypes)
+    train_dummy, test_dummy = one_hot(obj_train, obj_test)  # Train: (1460, 194) / Test: (1459, 159)
+    columns = train_dummy.columns
+    # print(list(set(columns) - set(test_dummy.columns)))  # <--- Shows difference in Series.
+    # More columns in Train set. All test are in train
 
     # Rejons one-hot with numeric
-    train_dataframe, test_dataframe = rejoin(train_dataframe, test_dataframe, train_dummy, test_dummy)
-    # train_dataframe.to_csv("temp.csv")
+    train, test = rejoin(train, test, train_dummy, test_dummy)
 
-    # # Finishes with a CSV
-    # train_dataframe.to_csv('featurized_train.csv', index=True)
-    # test_dataframe.to_csv('featurized_test.csv', index=True)
+    # Finishes with a CSV
+    train.to_csv('new_feat_train.csv', index=True)
+    test.to_csv('new_feat_test.csv', index=True)
